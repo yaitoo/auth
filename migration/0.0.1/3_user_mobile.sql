b0595b950d5e0f1e5336b9d50d344b3fa9db9e53
Lz <imlangzi@qq.com>
2024-04-15 17:47:30 +0800

    fix(sql): fixed mask size issue for user_email/user_mobile

--- migration/0.0.1/3_user_mobile.sql
+++ migration/0.0.1/3_user_mobile.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `<prefix>user_mobile` (
   `hash` VARCHAR(255) NOT NULL,
   `user_id` BIGINT NOT NULL,
-  `mask` VARCHAR(5) NOT NULL,
+  `mask` VARCHAR(125) NOT NULL,
   `is_verified` BIT(1) NOT NULL DEFAULT 0,
   `verified_at` DATETIME NULL,
   `created_at` DATETIME NOT NULL,
